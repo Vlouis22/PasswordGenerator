b604ad9c0529d7d23a69c5d3dfc3f4dc0c5a50e0
valerylouis <louisvalery2022@gmail.com>
2023-04-01 16:20:19 -0400

    "Updated"

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,38 +1,22 @@
 import random
-'''A password generator that generates a random password
- from the length of the user input '''
+def generate_password(length_of_password):
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    alpha_upper = alpha.upper()
+    special_char = '!@#$%^&*'
+    num = '0123456789'
+    # adding all possible characters
+    characters = (alpha + alpha_upper + special_char + num)
+    your_password = []
 
+    # loop to generate the password based on the length provided
+    for i in range(length_of_password):
+        passw = random.choice(characters)
+        # prevent duplicates
+        characters = characters.replace(passw, '')
+        your_password.append(passw)
 
-class PasswordGenerator:
-    # takes the length from the user
-    def __init__(self, length):
-        self.length = length
-        self.characters = characters
+    return ''.join(your_password)
 
-    # algorithm for generating the password
-    def generate_password(self):
-        # empty set that will take from the random module
-        your_password = []
-        # makes the password equal to the length of the user request
-        for ch in range(self.length):
-            # generates random characters
-            passw = random.randint(0, len(characters))
-            # append those characters to the empty password set
-            your_password.append(characters[passw])
-        # return the password set as strings
-        return ''.join(your_password)
-
-# This will take the length of the password the user request
 length_of_password = int(input("Length of password requested: "))
-alpha = 'abcdefghijklmnopqrstuvwxyz'
-alpha_upper = alpha.upper()
-special_char = '!@#$%^&*'
-num = '0123456789'
-# Adds up all the possibilities from letters, numbers, and special characters
-characters = (alpha + alpha_upper + special_char + num)
-
-# Use class and length of the password
-password_generator = PasswordGenerator(length_of_password)
-# get password from the password generator and print it
-password = password_generator.generate_password()
+password = generate_password(length_of_password)
 print(password)
